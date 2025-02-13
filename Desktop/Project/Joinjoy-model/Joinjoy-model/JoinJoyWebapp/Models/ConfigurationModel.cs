045e3100cd432ce3dc8a46b0e0ca5d7fe62ce24e
tintin19900 <119733857+tintin19900@users.noreply.github.com>
2025-02-13 19:59:02 +0700

    wrong PROJECT

--- Desktop/Project/Joinjoy-model/Joinjoy-model/JoinJoyWebapp/Models/ConfigurationModel.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace JoinJoyWebapp.Models;
-
-public class JwtSettings
-{
-    public string AccessCookie { get; set; } = "AccessToken";
-    public string RefreshCookie { get; set; } = "RefreshToken";
-}
